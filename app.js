9b9d4eb76d22ab9c01b61afd9f93a4c9d7a76c5b
Sasa Cvetkovic <cvetkovicsasahs@gmail.com>
2023-01-07 22:03:32 +0100

    add auth

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+require("express-async-errors");
+
+// express
+const express = require("express");
+const app = express();
+// database
+const connectDB = require("./db/connect");
+// routers
+const authRouter = require("./routes/authRoutes");
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/authController.js
@@ -0,0 +1,49 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { createTokenUser, createJWT } = require("../utils");
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = createTokenUser(user);
+  const jwt = createJWT({ payload: tokenUser })
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token:jwt });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  const tokenUser = createTokenUser(user);
+  const jwt = createJWT({ payload: tokenUser })
+
+  res.status(StatusCodes.OK).json({ user: tokenUser, token: jwt });
+};
+
+module.exports = {
+  register,
+  login,
+};
