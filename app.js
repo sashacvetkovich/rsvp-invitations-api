e4e48a75e654e7c17eaae47c34f4211c209833d0
Sasa Cvetkovic <sasa.cvetkovic@vegait.rs>
2023-02-01 15:41:01 +0100

    add upload image functionality

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 require("dotenv").config();
 require("express-async-errors");
-const cors = require("cors")
+const cors = require("cors");
 
 // express
 const express = require("express");
 const app = express();
+
+const fileUpload = require('express-fileupload');
+
+// cloudinary
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 // rest of the packages
 const cookieParser = require("cookie-parser");
+
 // database
 const connectDB = require("./db/connect");
+
 // routers
 const authRouter = require("./routes/authRoutes");
 const invitationRouter = require("./routes/invitationRoutes");
 const eventRouter = require("./routes/eventRoutes");
 const guestRouter = require("./routes/guestRoutes");
+const uploadRouter = require("./routes/uploadRoutes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -33,12 +47,14 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/invitations", invitationRouter);
 app.use("/api/v1/events", eventRouter);
 app.use("/api/v1/guest", guestRouter);
+app.use("/api/v1/upload", uploadRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
